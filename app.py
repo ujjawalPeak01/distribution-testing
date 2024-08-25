e501b9714ab595c221c9028d41a2f8a9e0ca2858
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2024-08-25 13:33:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from transformers import pipeline
+import time
+import random
 
 
 class InferlessPythonModel:
+    @staticmethod
+    def random_stop():
+        delay = random.uniform(5, 10)
+        time.sleep(delay)
 
     def initialize(self):
+        start_time = time.time()
         self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M",device=0)
+        InferlessPythonModel.random_stop()
         print("This is Initialize Code", flush=True)
+        tot = time.time() - start_time
+        print("Total Initialize Time: ", tot, flush=True)
 
     
     def infer(self, inputs):
+        start_time = time.time()
         prompt = inputs["prompt"]
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20, max_length=300)
         generated_txt = pipeline_output[0]["generated_text"]
+        InferlessPythonModel.random_stop()
+        tot = time.time() - start_time
+        print("Total Initialize Time: ", tot, flush=True)
         return {"generated_text": generated_txt}
 
     def finalize(self,args):
